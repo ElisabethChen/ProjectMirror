96720338be43231c22933917997ad33d0c0e6739
echen <echen@ug.kth.se>
2022-05-17 15:59:20 +0200

    print color to mirror

--- Assets/Script/TestScript.cs
+++ Assets/Script/TestScript.cs
@@ -14,7 +14,7 @@ public class TestScript : MonoBehaviour
 
     // my variables
     float t = 0;
-    int x = 30, y = 200;
+    int x = 0, y = 50;
     int width, height;
     Vector3 camPos;
     private int mirrorMask;
@@ -37,11 +37,12 @@ public class TestScript : MonoBehaviour
     void Update()
     {
         castAllRays();
+        // castRaysToMirrors();
     }
 
     private void setPixelColor(RaycastHit hit, Color color)
     {
-        color = new Color(1, 0, 1);
+        // color = new Color(1, 0, 1);
         // TODO: set the mirror pixel color as the reflected color
         Renderer hitRend = hit.collider.GetComponent<Renderer>();
         Texture2D hitTex = (Texture2D)hitRend.material.mainTexture;
@@ -50,12 +51,12 @@ public class TestScript : MonoBehaviour
         texCoord.y *= hitTex.height;
         // Debug.Log(texCoord.x);
         hitTex.SetPixel(Mathf.FloorToInt(texCoord.x), Mathf.FloorToInt(texCoord.y), color);
-        hitTex.SetPixel(Mathf.FloorToInt(texCoord.x+1), Mathf.FloorToInt(texCoord.y), color);
-        hitTex.SetPixel(Mathf.FloorToInt(texCoord.x), Mathf.FloorToInt(texCoord.y+1), color);
-        hitTex.SetPixel(Mathf.FloorToInt(texCoord.x+1), Mathf.FloorToInt(texCoord.y+1), color);
-        hitTex.SetPixel(Mathf.FloorToInt(texCoord.x-1), Mathf.FloorToInt(texCoord.y), color);
-        hitTex.SetPixel(Mathf.FloorToInt(texCoord.x), Mathf.FloorToInt(texCoord.y-1), color);
-        hitTex.SetPixel(Mathf.FloorToInt(texCoord.x-1), Mathf.FloorToInt(texCoord.y-1), color);
+        // hitTex.SetPixel(Mathf.FloorToInt(texCoord.x + 1), Mathf.FloorToInt(texCoord.y), color);
+        // hitTex.SetPixel(Mathf.FloorToInt(texCoord.x), Mathf.FloorToInt(texCoord.y + 1), color);
+        // hitTex.SetPixel(Mathf.FloorToInt(texCoord.x + 1), Mathf.FloorToInt(texCoord.y + 1), color);
+        // hitTex.SetPixel(Mathf.FloorToInt(texCoord.x - 1), Mathf.FloorToInt(texCoord.y), color);
+        // hitTex.SetPixel(Mathf.FloorToInt(texCoord.x), Mathf.FloorToInt(texCoord.y - 1), color);
+        // hitTex.SetPixel(Mathf.FloorToInt(texCoord.x - 1), Mathf.FloorToInt(texCoord.y - 1), color);
         // hitTex.SetPixel((int) texCoord.x, (int)texCoord.y, color);
         // hitTex.SetPixel((int) texCoord.x, (int)texCoord.y, new Color(1, 1, 0));
         incXXYY();
@@ -64,7 +65,7 @@ public class TestScript : MonoBehaviour
         // Debug.Log("x: " + hitTex.width + ", y: " + hitTex.height);
         // Debug.Log(hit.collider);
         Debug.Log("x: " + texCoord.x + ", y: " + texCoord.y);
-        // Debug.Log("x: " + x + ", y: " + y);
+        Debug.Log("x: " + x + ", y: " + y);
         // Debug.Log("x: " + xx + ", y: " + yy);
     }
 
@@ -90,7 +91,6 @@ public class TestScript : MonoBehaviour
             yy += 1;
         }
     }
-
 
     private void castAllRays()    // first recrusive iteration
     {
@@ -135,7 +135,52 @@ public class TestScript : MonoBehaviour
         }
 
     }
+    
     // private Color recRayRef(Ray ray, float length, int bounces)    // recrusive
+    private void castRaysToMirrors()
+    {
+        increaseMirrors();
+        // // Debug.Log("x: " + x + ", y: " + y);
+
+        // DEBUG: test one ray
+        // hitting red cube: x=175, y=200
+        // reflection hit blue cube: x=100, y=200
+        // reflection hit nothing: x=160, y=160
+        // x = 100;
+        // y = 150;
+        // DEBUG: test code for line
+        lineRenderer.positionCount = 1;
+        lineRenderer.SetPosition(0, camPos);
+
+        ray = Camera.main.ScreenPointToRay(new Vector3(x, y, 0));
+        RaycastHit hit;
+        // NOTE: does not use layer for Physics.Raycast() since it can´t detect 
+        //       if the ray hit some other object before hitting the mirror
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, maxLength))
+        {
+            // DEBUG: test code for line
+            lineRenderer.positionCount += 1;
+            lineRenderer.SetPosition(lineRenderer.positionCount - 1, hit.point);
+
+            if (hit.collider.tag == "Mirror")
+            {
+                float remainingLength = maxLength - Vector3.Distance(ray.origin, hit.point);
+                Color color = recRayRef(hit, remainingLength, maxBounces - 1);
+                // Debug.Log("fist ray hit mirror");
+                setPixelColor(hit, color);
+            }
+        }
+        else
+        {
+            // Debug.Log("first ray did not hit mirror");
+
+            // DEBUG: test code for line
+            lineRenderer.positionCount += 1;
+            lineRenderer.SetPosition(lineRenderer.positionCount - 1, ray.origin + ray.direction * maxLength);
+        }
+
+    }
+    
     private Color recRayRef(RaycastHit hit, float length, int bounces)    // recrusive
     {
         // NOTE: NOT DONE
@@ -168,7 +213,7 @@ public class TestScript : MonoBehaviour
                 lineRenderer.SetPosition(lineRenderer.positionCount - 1, hit.point);
 
                 // Debug.Log("reflected ray hit a object");
-                pixelColor = new Color(0, 1, 1);    // cyan = object // TODO: set object color
+                pixelColor = new Color(1, 0, 0);    // red = object // TODO: set object color
             }
         }
         else
@@ -198,6 +243,18 @@ public class TestScript : MonoBehaviour
             y += 1;
         }
         // }
+    }
+
+    private void increaseMirrors()
+    {
+        GameObject[] mirrors = GameObject.FindGameObjectsWithTag("Mirror");
+        foreach (GameObject mirror in mirrors)
+        {
+            Debug.Log(mirror);
+            Vector3 pos = mirror.transform.position;
+            Vector2 pos2 = Camera.main.WorldToScreenPoint(pos);
+            Debug.Log(mirror + ", " + pos2);
+        }
     }
 }
 
